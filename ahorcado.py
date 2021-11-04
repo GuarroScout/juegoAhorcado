0c90b35764389942928cbfb44b17e4e67f92fdbb
guarroscout <jordifguerrero@gmail.com>
2021-11-04 15:34:58 -0600

    1.3.0: El programa ya te imprime una palabra hecha con '- - -' significando cada guión un caracter de la palabra por adivinar

--- ahorcado.py
+++ ahorcado.py
@@ -7,11 +7,28 @@ def read():
     choose_word = []
     number_data = []
     ahorcado = []
+
     from random import randint as ran
     number_data = len(read_data)
     choose_word = ran(0, number_data-1)
     ahorcado = read_data[choose_word]
-    print(ahorcado)
+    ahorcad = ahorcado[:-1]
+        
+    print("\n¡Adivina la palabra!\n")
+
+    # Desarrollo de la list anónima con la palabra
+    hide_word = '- '
+    list_word = []
+    list_word = len(ahorcad)
+    for i in range(list_word-1):
+        hide_word = str(hide_word) + '- '
+    print(hide_word)
+
+    # Desarrollo del error
+    # add = input("Ingresa una letra: ")
+    # print(add)
+
+
 
 def run():
     read()
