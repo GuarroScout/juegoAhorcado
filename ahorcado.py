8149d743817980aae4f119ed8ac803c135a460da
guarroscout <jordifguerrero@gmail.com>
2021-11-04 14:59:28 -0600

    1.1.1: El programa ya logra imprimir una palabra aleatoria que está contenida en el archivo datos.txt

--- ahorcado.py
+++ ahorcado.py
@@ -2,9 +2,16 @@ def read():
     read_data = []
     with open("./data.txt", "r", encoding="utf-8") as r:
         for reading in r:
-            read_data.append(reading)
-    print(read_data)
-
+            read_data.append(reading)  
+    
+    choose_word = []
+    number_data = []
+    ahorcado = []
+    from random import randint as ran
+    number_data = len(read_data)
+    choose_word = ran(0, number_data-1)
+    ahorcado = read_data[choose_word]
+    print(ahorcado)
 
 def run():
     read()
